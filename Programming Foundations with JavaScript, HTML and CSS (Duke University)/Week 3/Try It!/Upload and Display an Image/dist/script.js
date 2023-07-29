c7bd60560660e32a0493af9f182be1cf07ca7f89
Dakota <76007168+DStyck93@users.noreply.github.com>
2023-07-29 13:27:47 -0500

    Add files via upload

--- Programming Foundations with JavaScript, HTML and CSS (Duke University)/Week 3/Try It!/Upload and Display an Image/dist/script.js
+++ Programming Foundations with JavaScript, HTML and CSS (Duke University)/Week 3/Try It!/Upload and Display an Image/dist/script.js
@@ -2,11 +2,5 @@ function upload(){
   var filename = document.getElementById("filePicker");
   var img = new SimpleImage(filename);
   var canvas = document.getElementById("canvas");
-  
-  // For some reason width = undefined.
-  // I assume this is an issue with the required library.
-  // This makes adjusting the size of the canvas to match the picture impossible.
-  var width = img.getWidth();
-  
   img.drawTo(canvas);
 }
